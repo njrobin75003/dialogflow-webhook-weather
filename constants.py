a5536b721a44fbb8acda1c40d15bc5a518380013
Nicolas Robin <45395611+njrobin75003@users.noreply.github.com>
2019-07-25 16:43:33 +0200

    First commit.

---
+++ constants.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Name:        constants.py
+# Purpose:     constants declarations
+#
+# Author:      Nicolas Jacques ROBIN
+#
+# Created:     24/07/2019
+# Copyright:   (c) Servier 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+# Filenames constants
+
+cnil_protection_filename = 'opencnil-autorites-de-protection-vd-20190124.xlsx'
+country_code_filename = 'country-codes.xlsx'
+protection_level_messages_filename = 'protection-level-messages.xlsx'
+eu_countries_filename = 'eu-countries.xlsx'
+
+# Debugging options
+
+print_logs = False
+print_header = False
+
+# Protection level messages
+data_protection_level_FR_col_index = 0
+data_protection_level_EN_col_index = 1
+message_col_index = 2
+comment1_index = 3
+comment2_index = 4
+
+# Protection level messages dictionary
+protection_level_messages_dictkey_1 = 'Data Protection Level - FR'
+protection_level_messages_dictkey_2 = 'Data Protection Level - EN'
+protection_level_messages_dictkey_3 = 'Message'
+protection_level_messages_dictkey_4 = 'Comment1'
+protection_level_messages_dictkey_5 = 'Comment2'
